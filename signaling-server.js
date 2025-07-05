794a45bb76e4e950170e052b707145db711d35fe
Iqbal Farhan Rasyid <iqfarryd@gmail.com>
2025-07-05 12:12:57 +0700

    uppp

--- signaling-server.js
+++ signaling-server.js
@@ -13,53 +13,48 @@ const io = new Server(server, {
 
 const PORT = process.env.PORT || 3000; // Railway akan set PORT
 
-// Mapping user id ke socket id
-const pesertaIdToSocket = {};
-const staffIdToSocket = {};
+// Mapping socket.id ke peserta (jika perlu tracking manual)
+const pesertaSockets = {};
+const staffSockets = {};
 
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
-  // Join ke room ujian tertentu
   socket.on("join-ujian-room", ({ ujian_id, peserta_id, staff }) => {
-    console.log("join-ujian-room", { ujian_id, peserta_id, staff });
     socket.join(`ujian_${ujian_id}`);
-    if (peserta_id) {
-      socket.peserta_id = peserta_id;
-      pesertaIdToSocket[peserta_id] = socket.id;
+    if (peserta_id && !staff) {
+      // peserta_id sekarang adalah socket.id peserta
+      pesertaSockets[socket.id] = ujian_id;
+      console.log("PESERTA join room:", socket.id, "ujian:", ujian_id);
     }
     if (staff) {
-      socket.isStaff = true;
-      staffIdToSocket[socket.id] = socket.id; // pakai socket.id sebagai staff_id
+      staffSockets[socket.id] = ujian_id;
+      console.log("STAFF join room:", socket.id, "ujian:", ujian_id);
     }
   });
 
-  // Staff meminta peserta mengirim stream
   socket.on("request-peserta-stream", ({ peserta_id, staff_id, ujian_id }) => {
-    console.log("request-peserta-stream", { peserta_id, staff_id, ujian_id });
+    // peserta_id sekarang adalah socket.id peserta
+    console.log("STAFF minta stream ke peserta socket.id:", peserta_id, "oleh staff:", staff_id);
     io.to(peserta_id).emit("staff-request-stream", { staff_id });
   });
 
-  // Peserta mengirim signal ke staff
   socket.on("peserta-signal", ({ to, from, signal, ujian_id }) => {
-    console.log("peserta-signal", { to, from, ujian_id });
-    if (to) {
-      io.to(to).emit("peserta-signal", { from: socket.id, signal });
-    }
+    // to = staff socket.id
+    console.log("PESERTA kirim signal ke staff:", to, "dari:", from);
+    io.to(to).emit("peserta-signal", { from, signal });
   });
 
-  // Staff mengirim signal balasan ke peserta
   socket.on("staff-signal", ({ to, from, signal, ujian_id }) => {
-    console.log("staff-signal", { to, from, ujian_id });
-    if (to) {
-      io.to(to).emit("staff-signal", { from: socket.id, signal });
-    }
+    // to = peserta socket.id
+    console.log("STAFF kirim signal ke peserta:", to, "dari:", from);
+    io.to(to).emit("staff-signal", { from, signal });
   });
 
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
-    if (socket.peserta_id) delete pesertaIdToSocket[socket.peserta_id];
-    if (socket.isStaff) delete staffIdToSocket[socket.id];
+    delete pesertaSockets[socket.id];
+    delete staffSockets[socket.id];
   });
 });
 
